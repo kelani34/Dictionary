dd8bf6c68943e122383fd2ccb5d8e014d88a275a
kelani34 <kelanitaiwo34@gmail.com>
2022-11-02 07:26:03 +0100

    Style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { NavLink } from 'react-router-dom'
 
-function Header() {
-  return (
-    <div>Header</div>
-  )
+const navigation = [
+  { name: 'Dictionary', href: '/dictionary'},
+  { name: 'Customers', href: '/customers'},
+  { name: 'Dictionary', href: '/dictionary'},
+  { name: 'Calendar', href: '/p'},
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
 
-export default Header
+export default function Header(props) {
+  return (
+    <>
+    <Disclosure as="nav" className="bg-purple-600">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <NavLink
+                        key={item.name}
+                        to={item.href}
+                        className={({isActive}) => {
+                          return 'px-3 py-2 rounded-md text-sm font-medium no-underline ' + (!isActive ? ' no-underline text-slate-300 hover:bg-purple-700 hover:text-white' : 'no-underline bg-purple-800 text-white')
+                        }}
+                      >
+                        {item.name}
+                      </NavLink>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="rounded-full bg-purple-600 p-1 text-slate-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+
+                <NavLink
+                key={item.name}
+                to={item.href}
+                className={({isActive}) => {
+                  return 'no-underline block px-3 py-2 rounded-md text-base font-medium ' + (!isActive ? ' no-underline text-slate-300 hover:bg-purple-700 hover:text-white' : 'no-underline bg-purple-800 text-white')
+                }}
+                >
+                {item.name}
+                </NavLink>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+    <div className='min-h-screen p-4 max-w-7xl mx-auto'>
+      {props.children}
+    </div>
+    </>
+  )
+}
---
+++ src/pages/Dictionary.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dictionary() {
+  return (
+    <div>Dictionary</div>
+  )
+}
+
+export default Dictionary
