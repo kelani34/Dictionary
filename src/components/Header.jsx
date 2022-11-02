f3f06cbcd27d69369abe5235cccdeae59d45ce9b
kelani34 <kelanitaiwo34@gmail.com>
2022-11-02 08:00:11 +0100

    Add: ogo name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,6 +48,15 @@ export default function Header(props) {
                   </div>
                 </div>
               </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="rounded-full bg-purple-600 p-1 text-slate-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <h3>Kehlani's Dictionary</h3>
+                </button>
+              </div>
             </div>
           </div>
 
