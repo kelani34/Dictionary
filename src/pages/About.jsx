fe0bd35fe70243bb99d1e48c5d75b18dfe809225
kelani34 <kelanitaiwo34@gmail.com>
2022-11-02 10:42:59 +0100

    Fix: About spacing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,15 +3,18 @@ import React from 'react'
 function About() {
   return (
     <>
-    <h1 className='text-lg font-bold text-center'>Kehlnai's Dictionary</h1>
+    <div className='text-center h-full flex flex-col space-y-10'>
+    <h1 className='text-lg font-bold text-center text-purple-800'>Kehlnai's Dictionary</h1>
 
-    <p>This is a dictionary Where yu can search av=cross range of words. This was built using React alongside tailwind CSS.</p>
+    <p>This is a dictionary Where yu can search across range of words. This was built using React alongside tailwind CSS.</p>
 
     <p>The Website fetches data from <a href='https://dictionaryapi.dev'>Dictionary api</a></p>
 
     <p>This website was fully completed on wednesday, 2nd November 2022</p>
 
-    <p> </p>
+    <p> Cheers! </p>
+
+    </div>
 
     </>
   )
