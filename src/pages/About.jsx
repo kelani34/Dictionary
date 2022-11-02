da334f3d40fa007cc10a8d040006862d49f93eb3
kelani34 <kelanitaiwo34@gmail.com>
2022-11-02 10:38:01 +0100

    Add: About Statement

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 function About() {
   return (
     <>
-    <h1 className='text-lg font-bold'>Kehlnai's Dictionary</h1>
+    <h1 className='text-lg font-bold text-center'>Kehlnai's Dictionary</h1>
+
+    <p>This is a dictionary Where yu can search av=cross range of words. This was built using React alongside tailwind CSS.</p>
+
+    <p>The Website fetches data from <a href='https://dictionaryapi.dev'>Dictionary api</a></p>
+
+    <p>This website was fully completed on wednesday, 2nd November 2022</p>
+
+    <p> </p>
 
     </>
   )
